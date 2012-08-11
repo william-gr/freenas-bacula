f06cf28e5783d6db8289c384a409e1fc9ca131af
William Grzybowski <william@agencialivre.com.br>
2012-08-11 08:11:20 -0300

    Workaround environment bug in 8.2.0

--- nanobsd/plugins/bacula_pbi/scripts/post-install.sh
+++ nanobsd/plugins/bacula_pbi/scripts/post-install.sh
@@ -8,4 +8,4 @@ cp ${bacula_pbi_path}/bacula ${bacula_pbi_path}/ix-bacula-sd /usr/local/etc/rc.d
 pw group add bacula
 pw user add bacula -g bacula -d ${bacula_pbi_path}/etc/bacula/home
 
-${bacula_pbi_path}/bin/python ${bacula_pbi_path}/baculaUI/manage.py syncdb --migrate --noinput
+env -i ${bacula_pbi_path}/bin/python ${bacula_pbi_path}/baculaUI/manage.py syncdb --migrate --noinput
