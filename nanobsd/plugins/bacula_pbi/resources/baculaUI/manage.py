74f62fa9f39cce2b082bfc540649fa799f8eea31
William Grzybowski <wg@FreeBSD.org>
2013-08-04 10:28:28 -0300

    Minor style fix

--- nanobsd/plugins/bacula_pbi/resources/baculaUI/manage.py
+++ nanobsd/plugins/bacula_pbi/resources/baculaUI/manage.py
@@ -5,10 +5,9 @@ import os
 HERE = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.join(HERE, ".."))
 
+from django.core.management import execute_from_command_line
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "baculaUI.settings")
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "baculaUI.settings")
-
-    from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
